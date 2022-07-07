d5d3bd0211efdd702adc9f5a67eb684117edbbf3
MateusCDev <mateus.cesararaujo@hotmail.com>
2022-07-07 12:57:19 -0300

    Aula de diretivas

--- my-first-projectls/src/app/app.component.ts
+++ my-first-projectls/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Meu primeiro Projeto em Angular DIO';
+export class AppComponent implements OnInit{
+  contador = 0;
+  nome = "Mateus";
+  text= "";
+
+  pessoas = [
+    {
+      nome: 'Mateus',
+      sobrenome: 'Cesar'
+    },
+    {
+      nome: 'Tereza',
+      sobrenome: 'Cristina'
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Eduarda'
+    }
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    let intervalo = setInterval(() => {
+      this.contador++;
+      if (this.contador === 10){
+        clearInterval(intervalo);
+      }
+    }, 1000)
+  }
+
+   clicou(nome:string): void {
+    console.log('clicou em mim', nome)
+  }
+
+
 }
